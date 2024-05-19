e6c9529d66c31d44a2436a82e0d7c5816c38df0b
Sure-sh554 <sureshrathod0113@gmail.com>
2024-05-19 15:36:44 +0530

    First program

---
+++ src/main/java/org/example/MaxAndMin.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class MaxAndMin {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5,6,7,8,9};
+
+        int min=arr[0];
+        int max=arr[0];
+
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]<min){
+                min=arr[i];
+            }
+            if (arr[i]>max)
+                max=arr[i];
+
+        }
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
